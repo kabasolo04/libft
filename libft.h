34330cae745ed63ef6581e7d29ef7acb81605813
kabasolo <koldobikaabasolo@gmail.com>
2026-06-01 13:05:02 +0200

    Headers Removed

--- libft.h
+++ libft.h
@@ -1,9 +1,9 @@
 #ifndef LIBFT_H
-# define LIBFT_H
+#define LIBFT_H
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
